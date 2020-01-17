9f0b5bd686a99c759c881516f12cc33bb06bb78b
kevinsherrell <k.sherrell.1028@gmail.com>
2020-01-17 17:56:46 -0600

    added signup and login button styles for desktop layout

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -8,7 +8,8 @@ class Header extends React.Component {
     state = {
         browserWidth: window.innerWidth,
         searchToggle: false,
-        searchMenuFilter: false
+        searchMenuFilter: false,
+        navMenuToggle: false
     }
 
     componentDidMount() {
@@ -32,6 +33,11 @@ class Header extends React.Component {
             searchMenuFilter: !this.state.searchMenuFilter
         })
     }
+    toggleNavMenu = () => {
+        this.setState({
+            navMenuToggle: !this.state.navMenuToggle
+        })
+    }
 
     render() {
         console.log("Browser Width: " + this.state.browserWidth)
@@ -42,24 +48,34 @@ class Header extends React.Component {
                     <div className="container grid">
                         <h3>trad'r</h3>
                         <form action="" className={`navForm flex`}>
-                            <div className="searchInput"
-                                 onClick={ this.state.browserWidth < 767 && !this.state.searchToggle ? this.toggleSearchMenu : undefined}>
+                            <div className={`searchInput ${this.state.searchToggle ? "hidden" : undefined}`}>
                                 <input type="text" placeholder={"Search"}
-                                       disabled={this.state.browserWidth < 767 && "disabled"}/>
+                                       onClick={this.state.browserWidth < 767 && !this.state.searchToggle ? this.toggleSearchMenu : undefined}/>
                                 <img src={search} alt="" className={"searchImage"}/>
                             </div>
                             <div className={`locationInput ${this.state.browserWidth < 767 && "hidden"}`}>
                                 <input type="text" placeholder={"Location"}/>
                                 <img src={location} alt=""/>
+                                <button className={this.state.browserWidth < 767 ? "hidden" : undefined}>Search</button>
+
                             </div>
-                            <button className={this.state.browserWidth < 767 && "hidden"}>Search</button>
+
                         </form>
-                        <ul className={`${this.state.browserWidth < 767 ? "hidden" : "flex"} `}>
+                        <ul className={`${this.state.browserWidth < 1024 ? "hidden" : "flex"} loginSignup`}>
                             {/*<li><a href="">How it works</a></li>*/}
-                            <li><a href="">Sign up</a></li>
-                            <li><a href="">Log in</a></li>
+                            <li className={"flex"}>
+                                {/*todo -- if authenticated is true Sign up is hidden and log in becomes log out; An avatar  is then placed to the right of the page which will be a link to the dashboard*/}
+                                <i className="material-icons">person_add</i>
+                                <a href="">Sign up</a>
+                            </li>
+                            <li className={"flex"}>
+                                <i className="material-icons">person</i>
+                                <a href="">Log in</a>
+                            </li>
+
                         </ul>
-                        <i className={`material-icons ${this.state.browserWidth > 767 && "hidden"}`}>menu</i>
+                        <i className={`material-icons ${this.state.browserWidth > 1023 && "hidden"} hamburger`}
+                           onClick={this.toggleNavMenu}>menu</i>
                     </div>
                 </nav>
 
@@ -81,16 +97,18 @@ class Header extends React.Component {
                                 <img src={location} alt=""/>
                             </div>
                             <div className="searchMenuControls">
-                                <button type={"button"} onClick={this.toggleSearchMenuFilter}>Filter <i className="material-icons">{this.state.searchMenuFilter ? "keyboard_arrow_up": "keyboard_arrow_down"}</i></button>
-                                <button className={this.state.searchMenuFilter ? "hidden": undefined}>Search</button>
+                                <button type={"button"} onClick={this.toggleSearchMenuFilter}>Filter <i
+                                    className="material-icons">{this.state.searchMenuFilter ? "keyboard_arrow_up" : "keyboard_arrow_down"}</i>
+                                </button>
+                                <button className={this.state.searchMenuFilter ? "hidden" : undefined}>Search</button>
                             </div>
                             <div className={`filterOptions ${!this.state.searchMenuFilter && "hidden"}`}>
                                 <div className="price flex">
                                     <label>Price</label>
                                     <div className={"priceInput flex"}>
-                                        <input type="text" placeholder={"Min"}/>
-                                        <p>To</p>
-                                        <input type="text" placeholder={"Max"}/>
+                                        $<input type="number" max={"999"} placeholder={"Min"}/>
+                                        {/*<p>To</p>*/}
+                                        $<input type="number" max={"999"} placeholder={"Max"}/>
                                     </div>
                                 </div>
                                 <div className="distance flex">
@@ -99,6 +117,17 @@ class Header extends React.Component {
                                         <input type="text" placeholder={"Miles"}/>
                                         {/*<p>mi.</p>*/}
                                     </div>
+                                </div>
+                                <div className="category flex">
+                                    <label>Category</label>
+                                    <select className={"categoryInput"}>
+                                        <option value="computers">Computers</option>
+                                        <option value="tablets">Tablets</option>
+                                        <option value="phones">Phones</option>
+                                        <option value="tvs">TVs</option>
+                                        <option value="appliances">Appliances</option>
+                                        <option value="vehicles">Vehicles</option>
+                                    </select>
                                 </div>
                                 <div className="sortBy flex">
                                     <label>Sort by</label>
@@ -109,10 +138,21 @@ class Header extends React.Component {
                                         <option value="Closest">Closest</option>
                                     </select>
                                 </div>
+
                                 <button>Search</button>
                             </div>
                         </form>
                     </div>
+                </div>
+                {/*Nav Menu*/}
+                <div
+                    className={`${this.state.browserWidth < 767 && this.state.navMenuToggle ? "navMenuOpen" : "navMenuClosed"}`}>
+                    <ul>
+                        <li>Sign up</li>
+                        <li>Log in</li>
+                        <li>FAQs</li>
+                        <li>Help/Contact</li>
+                    </ul>
                 </div>
             </header>
         )
